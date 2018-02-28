8c421e8f7d888fc55e9d4ef36f25fe66fbb39d05
Liy <810677019@qq.com>
2018-02-28 14:27:49 +0800

    Initial commit For Mr.Li for git by interlij

---
+++ src/com/mr/test/Mytest.java
@@ -0,0 +1,28 @@
+package com.mr.test;
+
+public class Mytest implements  Runnable{
+    @Override
+    public void run() {
+
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println("Helle World!");
+//    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+
+        new Thread( () -> System.out.println("In Java8, Lambda expression rocks !!") ).start();
+
+        System.out.println("ReCompile Ok!");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("start Thread!");
+            }
+        }).start();
+    }
+}
---
+++ src/com/mr/test/Woman.java
@@ -0,0 +1,5 @@
+package com.mr.test;
+
+public class Woman {
+
+}
